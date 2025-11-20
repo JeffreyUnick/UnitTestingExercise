1ca52729d12693911c8b2843f8f78ec46821bf6f
UNICK J <jeffreyodeloui@gmail.com>
2025-11-19 21:50:46 -0500

    Completed

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,7 +7,10 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] 
-        [InlineData(0,0,0,0)]//Add test data <-------
+        [InlineData(9,8,7,24)]//Add test data <-------
+        [InlineData(2,5,7,14)]
+        [InlineData(4,3,1,8)]
+        [InlineData(67,2345,500,2912)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -28,7 +31,11 @@ namespace UnitTestingExercise.Tests
         [Theory]
         [InlineData(7, 7, 0)]
         [InlineData(-9, 6, -15)]
-        [InlineData(9, -13, 22)]//Add test data <-------
+        [InlineData(9, -13, 22)]
+        [InlineData(78,9,69)]
+        [InlineData(987,789,198)]
+        [InlineData(67,23,44)]
+        [InlineData(56,34,22)]
         public void SubtractTest(int minuend, int subtrahend, int expected)
         {
             //Start Step 5 here:
@@ -43,7 +50,12 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(7, 3, 21)]
-        [InlineData(8, 0, 0)]//Add test data <-------
+        [InlineData(8, 0, 0)]
+        [InlineData(98,2,196)]
+        [InlineData(5,0,0)]
+        [InlineData(0,6,0)]
+        [InlineData(20,4,80)]
+        [InlineData(65,5,325)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -58,7 +70,12 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(70, 5, 14 )]
-        [InlineData(8, 4, 2 )]//Add test data <-------
+        [InlineData(8, 4, 2 )]
+        [InlineData(8,4,2)]
+        [InlineData(36,6,6)]
+        [InlineData(24,8,3)]
+        [InlineData(44,22,2)]
+        [InlineData(270,90,3)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
